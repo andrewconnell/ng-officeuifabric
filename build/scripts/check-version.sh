23201765761cc84eac78c20fec4b4c150cf7681f
Andrew Connell <me@andrewconnell.com>
2016-03-09 06:30:37 -0500

    added circle.yml

---
+++ build/scripts/check-version.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+ARG_DEFS=(
+  "--srcwd=(.*)"
+)
+
+function run {
+  echo $PWD
+  echo $SRCWD
+
+  # get version
+  VERSION="$(readJsonProp "package.json" "version")"
+
+
+  # get last tag
+  LAST_TAG=$(git describe --tags --abbrev=0)
+
+
+  # make sure version != last tag,
+  # `if so, no release needed so abort
+  if [ "$LAST_TAG" == "$VERSION" ]; then
+    echo "INFO: not a new version; aborting release script"
+    exit 1
+  else
+    echo "INFO: new version... latest tag is $LAST_TAG"
+    echo "INFO: version in package is $VERSION"
+    exit 0
+  fi
+}
+
+source $(dirname $0)/utils.inc
