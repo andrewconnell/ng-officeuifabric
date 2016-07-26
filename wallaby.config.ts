8b2d5379f6690a9df99fd6ab957e411f81626342
Artem Govorov <artem.govorov@gmail.com>
2016-07-26 22:08:47 +1000

    chore(test): wallaby.js config

--- wallaby.config.ts
+++ wallaby.config.ts
@@ -1,5 +1,5 @@
 import { IWallabyConfig, IWallabyEnvironment } from 'wallaby';
-import { BuildConfig } from './build';
+import { BuildConfig } from './build/config/build';
 
 class WallabyConfig implements IWallabyConfig {
 
@@ -8,27 +8,22 @@ class WallabyConfig implements IWallabyConfig {
    *                                copies to it's local cache.
    */
   public files: any = [
-    { instrument: false, pattern: BuildConfig.NODE_MODULES + '/phantomjs-polyfill/bind-polyfill.js'},
     { instrument: false, pattern: BuildConfig.NODE_MODULES + '/angular/angular.js'},
     { instrument: false, pattern: BuildConfig.NODE_MODULES + '/angular-mocks/angular-mocks.js'},
     { instrument: false, pattern: BuildConfig.NODE_MODULES + '/jquery/dist/jquery.min.js'},
-    // { instrument: false, pattern: BuildConfig.NODE_MODULES + '/pickadate/lib/picker.js'},
-    // { instrument: false, pattern: BuildConfig.NODE_MODULES + '/pickadate/lib/picker.date.js'},
+    { instrument: false, pattern: BuildConfig.NODE_MODULES + '/pickadate/lib/picker.js'},
+    { instrument: false, pattern: BuildConfig.NODE_MODULES + '/pickadate/lib/picker.date.js'},
     { instrument: false, pattern: BuildConfig.NODE_MODULES + '/jasmine-jquery/lib/jasmine-jquery.js'},
     { instrument: false, pattern: 'src/core/jquery.phantomjs.fix.js'},
-    'src/**/*.ts',
+    {pattern: 'src/**/*.ts', load: false},
     '!src/**/*.spec.ts'
   ];
 
   /**
    * @property  {string[]}  tests - Collection of files that include tests.
    */
-  public tests: string[] = [
-    //'src/**/*.spec.ts'
-    // 'src/components/*/!(*.spec|contextualMenu|navbarDirective).ts',
-    'src/components/navbar/navbarDirective.ts'
-    // 'src/components/contextualmenu/contextualMenu.ts',
-    // 'src/components/*/*.spec.ts'
+  public tests: any[] = [
+    {pattern: 'src/**/*.spec.ts', load: false}
   ];
 
   public env: IWallabyEnvironment = <IWallabyEnvironment>{
@@ -43,6 +38,26 @@ class WallabyConfig implements IWallabyConfig {
    */
   public compilers: any = {
     'src/**/*.ts': this.wallaby.compilers.typeScript()
+  };
+
+  public postprocessor: any = require('wallaby-webpack')({
+    entryPatterns: [
+      'src/core/components.js',
+      'src/core/core.js',
+      'src/**/*.spec.js'
+    ],
+    externals: {
+      'angular': 'angular'
+    },
+    resolve: {
+      extensions: ['', '.js', '.ts'],
+      root: './src'
+    }
+  });
+
+  public setup: any = function () {
+    // required to trigger test loading
+    window['__moduleBundler'].loadTests();
   };
 
   constructor(private wallaby: any) { }
